6a06a8d1d9e100601153d37d4fa80d3b5c8cde63
bill-burgess <wmhburgess@gmail.com>
2017-07-19 17:26:53 +1200

    createUser/ create/Profile

--- db.js
+++ db.js
@@ -2,7 +2,8 @@
 module.exports = {
   getUser: getUser,
   getUsers: getUsers,
-  getProfiles: getProfiles
+  getProfiles: getProfiles,
+  createUser: createUser
 }
 
 function getUsers (connection) {
@@ -15,4 +16,27 @@ function getProfiles (connection) {
 
 function getUser (id, connection) {
   return connection('users').where('id', id)
+}
+
+function createUser(newUser, newProfile, connection){
+  var x = newProfile
+  console.log(x)
+  return connection('users')
+  .insert(newUser)
+  .then(function(ids) {
+    var id = ids[0]
+    return createProfile(x, id, connection)
+ })
+
+}
+
+function createProfile(newProfile, userId, connection) {
+  var profile = {
+      bio: newProfile.bio,
+      img: newProfile.img,
+      user_id: userId
+    }
+    console.log(profile)
+    return connection('profiles')
+      .insert(profile)
 }
--- routes/index.js
+++ routes/index.js
@@ -20,32 +20,32 @@ router.get('/add', function(req, res) {
 
 
 router.post('/add', function(req, res) {
-      // db.getUsers(req.app.get('connection'))
-      // console.log(req.body.name);
-      //         knex('wombles').insert(req.body).then(function(){
-                res.redirect("/")
-    // })
+    var newUser = {
+      name: req.body.name,
+      email: req.body.email
+    }
+    var newProfile = {
+      img : req.body.img,
+      bio : req.body.bio
+    }
+    db.createUser(newUser, newProfile, req.app.get('connection'))
+    // console.log(req.body.name);
+    res.redirect("/")
 
 })
-
 
 
 router.get('/:id', function (req, res) {
   db.getProfiles(req.app.get('connection'))
-    .where({'profile_id': req.params.id})
+    .where({'user_id': req.params.id}) // this was the change
     .join('users','id', '=', req.params.id)
     .then(function(profile){
-        console.log(profile)
+        console.log("A" + profile)
         res.render('profile', profile[0])
     })
     .catch(function (err) {
        res.status(500).send('DATABASE ERROR: ' + err.message)
-     })
+    })
 })
-
-
-
-
-
 
 module.exports = router
